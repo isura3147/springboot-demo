146fae372b572e659417e9b8e6bc30f7e802f372
isura <isura3147@gmail.com>
2025-11-03 22:33:37 +0530

    add more customer apis

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package edu.icet.controller;
 
 
+import edu.icet.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/customer")
@@ -19,4 +22,24 @@ public class CustomerController {
         return 23;
     }
 
+    @GetMapping("/details")
+    public Customer getCustomer() {
+        return new Customer(
+                "1",
+                "Jackson",
+                "Panadura",
+                1000.000
+        );
+    }
+
+    ArrayList<Customer> customerList = new ArrayList<Customer>();
+
+    @GetMapping("/allCustomers")
+    public ArrayList<Customer> getCustomerList() {
+        customerList.add(new Customer("1", "Jack", "Manhatten", 100.00));
+        customerList.add(new Customer("2", "Jill", "New York", 200.00));
+        customerList.add(new Customer("3", "Sarah", "Statten Island", 300.00));
+        customerList.add(new Customer("4", "Jason", "Golden State", 400.00));
+        return customerList;
+    }
 }
